16bbe310d59d22aae3c165d76ff67a8cdb2baa26
Ealden Esto E. Escanan <ealden@gmail.com>
2012-06-23 07:21:34 +0800

    Use AndroidAnnotations in project
    There does seem to be a problem with packaging the app inside IDEA, as it causes the app in the simulator to crash.  However, using `gradle androidInstall` works perfectly.
    
    Signed-off-by: Ealden Esto E. Escanan <ealden@gmail.com>

--- src/main/java/net/ealden/gradle/MainActivity.java
+++ src/main/java/net/ealden/gradle/MainActivity.java
@@ -2,14 +2,8 @@ package net.ealden.gradle;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.googlecode.androidannotations.annotations.EActivity;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+@EActivity(R.layout.main)
+public class MainActivity extends Activity {
 }
